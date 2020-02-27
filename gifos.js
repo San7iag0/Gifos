ec7c843d5755016ab7e63ee5fdb641dfcbab9ca9
santiago <ttsbpn@gmail.com>
2020-02-26 19:35:39 -0500

    Elegir tema
    Botones de estilade background

---
+++ gifos.js
@@ -0,0 +1,9 @@
+function elegirTema(){
+    document.getElementById("dropD").classList.toggle("show");
+}
+function light() {
+document.body.style.backgroundColor = '#1f004d';
+}
+function dark(){
+document.body.style.backgroundColor = '#e6f2ff';
+}
