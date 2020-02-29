9f7dd700a0043b1ea6cc8b34ab46be87fdbec282
santiago <ttsbpn@gmail.com>
2020-02-28 22:51:14 -0500

    upload screen
    created upload screen

--- gifos.js
+++ gifos.js
@@ -2,7 +2,8 @@ function elegirTema(){
     document.getElementById("dropD").classList.toggle("show");
 }
 function light() {
-document.body.style.backgroundColor = '#1f004d';
+document.body.style.backgroundColor = '#CCA6C9';
+// document.getElementById("logoImg").href = "./gifOS_UI 3/assets/gifOF_logo_dark.png";
 }
 function dark(){
 document.body.style.backgroundColor = '#e6f2ff';
