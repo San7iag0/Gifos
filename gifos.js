df49d35f20bef584414376e7cdaa121e5f2f0fb8
santiago <ttsbpn@gmail.com>
2020-03-01 00:35:52 -0500

    background change
    background change

--- gifos.js
+++ gifos.js
@@ -1,10 +1,17 @@
 function elegirTema(){
-    document.getElementById("dropD").classList.toggle("show");
+	document.getElementById("dropD").classList.toggle("show");
 }
 function light() {
-document.body.style.backgroundColor = '#CCA6C9';
-// document.getElementById("logoImg").href = "./gifOS_UI 3/assets/gifOF_logo_dark.png";
+	document.body.style.backgroundColor = '#fff4fd';
+	
+	
 }
 function dark(){
-document.body.style.backgroundColor = '#e6f2ff';
+	document.body.style.backgroundColor = '#110038';
+	document.getElementById('SailorDbutton').style.background = '#EE3EFE';
+    document.getElementById('SailorNbutton').style.background = '#EE3EFE';
+    
 }
+
+
+// background: #EE3EFE;
