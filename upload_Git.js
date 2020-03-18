a17cdf2ae5875f57f0bd1e6a843f112a4dcb6cc3
santiago <ttsbpn@gmail.com>
2020-03-17 22:42:32 -0500

    capturar
    capturar

--- upload_Git.js
+++ upload_Git.js
@@ -1,3 +1,9 @@
+// function comenzarGifo(){
+//     document.querySelector('.comenzarGifo').style.display = 'none';
+// }
+
 function comenzarGifo(){
-    
+    document.querySelector('.content').style.display = 'none';
+    // document.querySelector('.sugerencias').style.display = 'none';
+    // document.querySelector('misGifosBox').style.display = 'none';
 }
