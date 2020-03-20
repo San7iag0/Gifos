5689d0b2b1d1cddc156a6d9c2d2800e67c8e59a5
santiago <ttsbpn@gmail.com>
2020-03-19 23:10:14 -0500

    Video
    Still working on the submitted video

--- Scripts/uploadVideo.js
+++ Scripts/uploadVideo.js
@@ -1,6 +1,20 @@
+
 document.getElementById('comenzarGifo').onclick = function (){
 	document.querySelector('.crearGifos').style.display = 'none';
 	document.querySelector('.misGifos').style.display = 'none';
 	document.querySelector('.misGifosBox').style.display = 'none';
 	
 	document.querySelector('.videoParent').style.display = 'grid';
+}
+	
+
+function capturar(){
+	navigator.mediaDevices.getUserMedia({
+		audio:false,
+		video:true
+	}).then(function(stream){
+		video.srcObject = stream;
+		video.play()
+	}).catch(console.error);
+}
+	
