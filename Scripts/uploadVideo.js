b52807f21a52b0d151787bdcee0b7cc5e285fb98
santiago <ttsbpn@gmail.com>
2020-03-20 23:40:39 -0500

    video upload
    btns for video upload

--- Scripts/uploadVideo.js
+++ Scripts/uploadVideo.js
@@ -19,13 +19,13 @@ function capturar(){
 
 const video = document.getElementById('video');
 
-// function capturar(){
-// 	navigator.mediaDevices.getUserMedia({
-// 			audio:false,
-// 			video:true
-// 		}).then(function(stream){
-// 			video.srcObject = stream;
-// 			video.play()
-// 		}).catch(console.error);
-// }
+function capturar(){
+	navigator.mediaDevices.getUserMedia({
+			audio:false,
+			video:true
+		}).then(function(stream){
+			video.srcObject = stream;
+			video.play()
+		}).catch(console.error);
+}
 	
