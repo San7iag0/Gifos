42ed5db3beba1b67b50a22b4878a58bff03b3353
santiago <ttsbpn@gmail.com>
2020-03-20 22:12:26 -0500

    upload
    *upload bottons

--- Scripts/uploadVideo.js
+++ Scripts/uploadVideo.js
@@ -3,19 +3,29 @@ document.getElementById('comenzarGifo').onclick = function (){
 	document.querySelector('.crearGifos').style.display = 'none';
 	document.querySelector('.misGifos').style.display = 'none';
 	document.querySelector('.misGifosBox').style.display = 'none';
+	document.querySelector('.btnlisto').style.display = 'none';
+	document.querySelector('.recording').style.display = 'none';
 	
 	document.querySelector('.videoParent').style.display = 'grid';
 }
 
+function capturar(){
+	document.querySelector('.btnlisto').style.display = 'grid';
+	document.querySelector('.btncapturar').style.display = 'none';
+	document.querySelector('.camera').style.display = 'none';
+	document.querySelector('.recording').style.display = 'grid';
+
+}
+
 const video = document.getElementById('video');
 
-function capturar(){
-	navigator.mediaDevices.getUserMedia({
-			audio:false,
-			video:true
-		}).then(function(stream){
-			video.srcObject = stream;
-			video.play()
-		}).catch(console.error);
-}
+// function capturar(){
+// 	navigator.mediaDevices.getUserMedia({
+// 			audio:false,
+// 			video:true
+// 		}).then(function(stream){
+// 			video.srcObject = stream;
+// 			video.play()
+// 		}).catch(console.error);
+// }
 	
