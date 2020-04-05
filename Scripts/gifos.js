08157633a8988fa6a3e7214003a51079d0b024e4
santiago <ttsbpn@gmail.com>
2020-04-04 21:09:57 -0500

    gifs title
    gifs title

--- Scripts/gifos.js
+++ Scripts/gifos.js
@@ -107,7 +107,7 @@ window.onload = () => {
 	// let contenedor = document.getElementById("content");
 	createBoxesWithGifts('http://api.giphy.com/v1/gifs/search?q= dog &api_key=' + apiKey, "content");
 	
-	createBoxesWithGifts("https://api.giphy.com/v1/gifs/random? &api_key=" +  apiKey + "&limit=2", 'advice');
+	createBoxesWithGifts('http://api.giphy.com/v1/gifs/search?q= agusta &api_key=' + apiKey + "&limit=2", 'advice');
 	createBoxesWithGifts('http://api.giphy.com/v1/gifs/search?q= vegeta &api_key=' + apiKey + "&limit=2", 'advice1');
 	createBoxesWithGifts('http://api.giphy.com/v1/gifs/search?q= goku &api_key=' + apiKey + "&limit=2", 'advice2');
 	createBoxesWithGifts('http://api.giphy.com/v1/gifs/search?q= broly &api_key=' + apiKey + "&limit=2", 'advice3');
@@ -118,12 +118,14 @@ window.onload = () => {
 
 function createBoxesWithGifts(url, contentId) {
 	fetch(url)
-	// promess #1
 		.then(response => { 
 			return response.json();
 		})
-	// Promess #2
 		.then(dataList => {
+			for(var i = 0; i < dataList.length; i++){
+			let text = document.getElementById('textadvice').innerHTML = dataList.data[i].title;
+			console.log(text);
+			}
 			printGiftBoxes(dataList.data, contentId);
 		return data;
 		})
