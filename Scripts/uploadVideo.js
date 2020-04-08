ace74f78ed65ce01920a0fda5c510cdcfc25a261
santiago <ttsbpn@gmail.com>
2020-04-07 21:12:23 -0500

    .

--- Scripts/uploadVideo.js
+++ Scripts/uploadVideo.js
@@ -7,9 +7,28 @@ document.getElementById('comenzarGifo').onclick = function (){
 	document.querySelector('.recording').style.display = 'none';
 	
 	document.querySelector('.videoParent').style.display = 'grid';
+
+	//function to start the streaming 
+	navigator.mediaDevices.getUserMedia({
+		audio:false,
+		video:{
+			height:{ max: 434}
+	
+		}
+	}).then(function(stream){
+		video.srcObject = stream;
+		video.play()
+	}).catch(console.error);
+	// navigator.mediaDevices.getUserMedia({ 
+	// 	video: true,
+	// 	Audio: false
+	// }).then(function(camera) {
+	// 	callback(camera);
+	// }).catch(function(error) {
+	// 	alert('Unable to capture.');
+	// 	console.error(error);
+	// });
 }
-
-
 
 const video = document.getElementById('video');
 
@@ -20,20 +39,17 @@ function capturar(){
 	document.querySelector('.recording').style.display = 'grid';
 
 	document.querySelector('.titleV').innerHTML = 'Capturando Tu Guifo';
-	navigator.mediaDevices.getUserMedia({
-			audio:false,
-			video:{
-				height:{ max: 434}
-
-			}
-		}).then(function(stream){
-			video.srcObject = stream;
-			video.play()
-		}).catch(console.error);
+	
 }
+
+document.getElementById('btnlisto').onclick = function (){
+	stopRecording();
+}
+
 
 function stopRecording(){
 	document.querySelector('.titleV').innerHTML = 'Vista previa';
-	
+	Image.src = URL.created.createObjectURL(recorder.getBlob());
+	recorder.camera.stop();
 }
 	
